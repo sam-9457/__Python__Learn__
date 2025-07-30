ca8fc13a9f2ef5d1ac7fbb18f27ee17bba47d652
張耀升 <badbloodlover1@gmail.com>
2025-07-30 03:57:50 +0000

    modify

--- lesson7/lesson7_5.py
+++ lesson7/lesson7_5.py
@@ -11,27 +11,29 @@ if not args.name:
 else:
     name = args.name
 freqency = args.frequency
-print("========猜數字遊戲=========\n\n")
-min = 1
-max = 100
-count = 0
-target = random.randint(min,max)
-while(True):
-  keyin = int(input(f"猜數字範圍{min}~{max}:"))
-  count += 1
-  if(keyin>=min and keyin<=max):
-    if target == keyin:
-      print(f"賓果!猜對了, 答案是:{target}")
-      print(f"你共猜了{count}次\n")
-      break
-    elif(keyin > target):
-      print(f"猜錯了!再小一點")
-      max = keyin - 1
-    else:
-      print(f"猜錯了!再大一點")
-      min = keyin + 1
-    print(f"您已經猜{count}次\n")
-  else:
-    print("請輸入提示範圍內的數字\n")
+for i in range(freqency):
+    print(f"========猜數字遊戲第{i+1}次=========\n\n")
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min,max)
+    while(True):
+        print(target)
+        keyin = int(input(f"猜數字範圍{min}~{max}:"))
+        count += 1
+        if(keyin>=min and keyin<=max):
+            if target == keyin:
+                print(f"賓果!猜對了, 答案是:{target}")
+                print(f"你共猜了{count}次\n")
+                break
+            elif(keyin > target):
+                print(f"猜錯了!再小一點")
+                max = keyin - 1
+            else:
+                print(f"猜錯了!再大一點")
+                min = keyin + 1
+            print(f"{name}已經猜{count}次\n")
+        else:
+            print("請輸入提示範圍內的數字\n")
 
-print("遊戲結束")    
+    print(f"遊戲結束,{name},共玩了{freqency}次")    
