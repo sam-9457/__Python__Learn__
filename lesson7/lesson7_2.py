3073a2c4c7d1823d073b8d79e7ad5281afb8b19f
張耀升 <badbloodlover1@gmail.com>
2025-07-30 02:42:43 +0000

    python

--- lesson7/lesson7_2.py
+++ lesson7/lesson7_2.py
@@ -1,1 +1,7 @@
-print("Python")
+import argparse
+
+parser = argparse.ArgumentParser(description="要求使用者輸入姓名")
+parser.add_argument("name", help ="請輸入姓名")
+args = parser.parse_args()
+
+print(f"您的姓名是:{args.name}")
