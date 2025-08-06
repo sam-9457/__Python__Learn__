d0ca3671dd3415fad2ca27c6cbd55db3470ce878
張耀升 <badbloodlover1@gmail.com>
2025-08-06 03:29:39 +0000

    modify

---
+++ lesson10/lesson10_2.py
@@ -0,0 +1,8 @@
+def main():
+    player = [
+        {"name": "", "play_times": 0, "roler": ""},
+        {"name": "", "play_times": 0, "roler": ""}
+    ]
+    print("1.player初始化完成")
+if __name__ == "__main__":
+    main()
