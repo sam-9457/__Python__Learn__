9da2b61e408f57579531125fae7e4f0e4a77e110
張耀升 <badbloodlover1@gmail.com>
2025-08-11 04:01:40 +0000

    modify

--- lesson12/lesson12_0.py
+++ lesson12/lesson12_0.py
@@ -1,16 +1,20 @@
-import random
+import random, time
 
-answer = [
-    "hi",
-    "I don`t know",
-    "HaHa"
-]
-
-while True:
-    player = input("")
-    if player != "Bye!":
-        ai = random.randint(0, 2)
-        print(f"Ai: {answer[ai]}")
-    else:
-        print("Ai: Bye!")
-        break
+def printt():
+    print("Premium lua language provide!")
+    print("Loading")
+    time.sleep(1)
+    a = ["local", "function", "for", "while"]
+    b = ["num", "random", "part", "i"]
+    c = ["=", "()", "in", "true"]
+    d = ["workspace", "", "pairs", "do"]
+    e = ["", "", "(folder:GetChildren()) do", ""]
+    for r in range(10):
+        ai1 = random.randint(0,3)
+        ai2 = random.randint(0,3)
+        ai3 = random.randint(0,3)
+        ai4 = random.randint(0,3)
+        ai5 = random.randint(0,3)
+        print(f"\n{a[ai1]} {b[ai2]} {c[ai3]} {d[ai4]} {e[ai5]}")
+        time.sleep(.3)
+printt()
